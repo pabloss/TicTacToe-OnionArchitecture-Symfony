93e76cf0dcab38ec75712dc3b759afe195d78bff
Paweł Zimnowodzki <pawel.zimnowodzki@gmail.com>
2019-01-23 11:39:04 +0100

    Doctrine Migration

---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20190123100816.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace App\Infrastructure\Persistence\Doctrine\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190123100816 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE player (id INT AUTO_INCREMENT NOT NULL, game_id INT DEFAULT NULL, symbol VARCHAR(255) NOT NULL, value_object LONGTEXT NOT NULL COMMENT \'(DC2Type:object)\', INDEX IDX_98197A65E48FD905 (game_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE history (id INT AUTO_INCREMENT NOT NULL, item VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, value_object LONGTEXT NOT NULL COMMENT \'(DC2Type:object)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE game (id INT AUTO_INCREMENT NOT NULL, history_id INT DEFAULT NULL, value_object LONGTEXT NOT NULL COMMENT \'(DC2Type:object)\', UNIQUE INDEX UNIQ_232B318C1E058452 (history_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE player ADD CONSTRAINT FK_98197A65E48FD905 FOREIGN KEY (game_id) REFERENCES game (id)');
+        $this->addSql('ALTER TABLE game ADD CONSTRAINT FK_232B318C1E058452 FOREIGN KEY (history_id) REFERENCES history (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE game DROP FOREIGN KEY FK_232B318C1E058452');
+        $this->addSql('ALTER TABLE player DROP FOREIGN KEY FK_98197A65E48FD905');
+        $this->addSql('DROP TABLE player');
+        $this->addSql('DROP TABLE history');
+        $this->addSql('DROP TABLE game');
+    }
+}
