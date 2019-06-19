fa774862d413ebeac388ce3b4859cf2a2460049b
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2019-06-19 22:13:18 +0200

    unit test for Take Tile Service

--- tests/unit/TakeTileServiceTest.php
+++ tests/unit/TakeTileServiceTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Tests\unit;
 
 use App\Core\Application\Service\TakeTileService;
+use App\Core\Domain\Model\TicTacToe\Exception\NotAllowedSymbolValue;
 use App\Core\Domain\Model\TicTacToe\Game\Board;
 use App\Core\Domain\Model\TicTacToe\Game\Game;
 use App\Core\Domain\Model\TicTacToe\Game\HistoryInterface;
@@ -11,6 +12,7 @@ use App\Core\Domain\Model\TicTacToe\Game\Player;
 use App\Core\Domain\Model\TicTacToe\ValueObject\Symbol;
 use App\Core\Domain\Model\TicTacToe\ValueObject\Tile;
 use PHPUnit\Framework\TestCase;
+use Prophecy\Prophecy\ObjectProphecy;
 
 /**
  * Class TakeTileServiceTest
@@ -21,41 +23,41 @@ class TakeTileServiceTest extends TestCase
     /**
      * @test
      */
-    public function board()
+    public function initBoard()
     {
-        $playerXProphecy = $this->prophesize(Player::class);
-        $playerOProphecy = $this->prophesize(Player::class);
-        $playerOProphecy->uuid()->willReturn(0);
-        $playerOProphecy->symbolValue()->willReturn(Symbol::PLAYER_0_SYMBOL);
-        $playerOProphecy->willBeConstructedWith([
-            new Symbol(Symbol::PLAYER_0_SYMBOL),
-            0
-        ]);
-        $playerXProphecy->uuid()->willReturn(1);
-        $playerXProphecy->symbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
+        list($playerXProphecy, $playerOProphecy) = $this->preparePlayers();
         $tileProphecy = $this->prophesize(Tile::class);
-        $playerXProphecy->willBeConstructedWith([
-            new Symbol(Symbol::PLAYER_X_SYMBOL),
-            1
-        ]);
+
+        $boardProphecy = $this->prophesize(Board::class);
+        $boardProphecy->getPlayer($tileProphecy->reveal())->willReturn($playerXProphecy->reveal());
+
+        $gameProphecy = $this->prepareGame($boardProphecy, $playerOProphecy, $playerXProphecy);
+
+        self::assertSame($boardProphecy->reveal(), $gameProphecy->reveal()->board());
+    }
+
+    /**
+     * @test
+     */
+    public function markBoard()
+    {
+        list($playerXProphecy, $playerOProphecy) = $this->preparePlayers();
+        $tileProphecy = $this->prophesize(Tile::class);
+
         $boardProphecy = $this->prophesize(Board::class);
         $boardProphecy->getPlayer($tileProphecy->reveal())->willReturn($playerXProphecy->reveal());
         $boardProphecy->mark($tileProphecy->reveal(), $playerXProphecy->reveal())->shouldBeCalled();
-        $gameProphecy = $this->prophesize(Game::class);
-        $gameProphecy->board()->willReturn($boardProphecy->reveal());
-        $gameProphecy->players()->willReturn([
-            $playerOProphecy->reveal(),
-            $playerXProphecy->reveal(),
-        ]);
+
+        $gameProphecy = $this->prepareGame($boardProphecy, $playerOProphecy, $playerXProphecy);
+
         $historyProphecy = $this->prophesize(HistoryInterface::class);
         $historyProphecy->lastItem($gameProphecy->reveal())->willReturn(null);
-        $historyProphecy->saveTurn($playerXProphecy->reveal(), $tileProphecy->reveal(), $gameProphecy->reveal());
         $historyProphecy->getStartingPlayerSymbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
+        $this->prepareSavingTurn($historyProphecy, $playerXProphecy, $tileProphecy, $gameProphecy);
+
         $service = new TakeTileService($gameProphecy->reveal(), $historyProphecy->reveal());
-
-        self::assertSame($boardProphecy->reveal(), $gameProphecy->reveal()->board());
-
         $service->takeTile($playerXProphecy->reveal(), $tileProphecy->reveal());
+
         self::assertNotEmpty($gameProphecy->reveal()->board()->getPlayer($tileProphecy->reveal()));
         self::assertSame($playerXProphecy->reveal(), $gameProphecy->reveal()->board()->getPlayer($tileProphecy->reveal()));
     }
@@ -65,7 +67,33 @@ class TakeTileServiceTest extends TestCase
      */
     public function hasError()
     {
-        $playerXProphecy = $this->prophesize(Player::class);
+        list($playerXProphecy, $playerOProphecy) = $this->preparePlayers();
+        $tileProphecy = $this->prophesize(Tile::class);
+
+        $boardProphecy = $this->prophesize(Board::class);
+
+        $gameProphecy = $this->prepareGame($boardProphecy, $playerOProphecy, $playerXProphecy);
+        $gameProphecy->addError(Game::GAME_STARTED_BY_PLAYER0_ERROR, $playerOProphecy->reveal())->shouldBeCalled();
+        $gameProphecy->errors()->willReturn(Game::GAME_STARTED_BY_PLAYER0_ERROR);
+
+        $historyProphecy = $this->prophesize(HistoryInterface::class);
+        $historyProphecy->lastItem($gameProphecy->reveal())->willReturn(null);
+        $historyProphecy->getStartingPlayerSymbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
+        $this->prepareSavingTurn($historyProphecy, $playerOProphecy, $tileProphecy, $gameProphecy);
+
+        $service = new TakeTileService($gameProphecy->reveal(), $historyProphecy->reveal());
+        $service->takeTile($playerOProphecy->reveal(), $tileProphecy->reveal());
+
+        self::assertTrue($service->hasError(Game::GAME_STARTED_BY_PLAYER0_ERROR));
+    }
+
+
+    /**
+     * @return array
+     * @throws NotAllowedSymbolValue
+     */
+    private function preparePlayers(): array
+    {
         $playerOProphecy = $this->prophesize(Player::class);
         $playerOProphecy->uuid()->willReturn(0);
         $playerOProphecy->symbolValue()->willReturn(Symbol::PLAYER_0_SYMBOL);
@@ -73,68 +101,42 @@ class TakeTileServiceTest extends TestCase
             new Symbol(Symbol::PLAYER_0_SYMBOL),
             0
         ]);
+        $playerXProphecy = $this->prophesize(Player::class);
         $playerXProphecy->uuid()->willReturn(1);
         $playerXProphecy->symbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
-        $tileProphecy = $this->prophesize(Tile::class);
         $playerXProphecy->willBeConstructedWith([
             new Symbol(Symbol::PLAYER_X_SYMBOL),
             1
         ]);
-        $boardProphecy = $this->prophesize(Board::class);
-        $gameProphecy = $this->prophesize(Game::class);
-        $gameProphecy->board()->willReturn($boardProphecy->reveal());
-        $gameProphecy->players()->willReturn([
-            $playerOProphecy->reveal(),
-            $playerXProphecy->reveal(),
-        ]);
-        $gameProphecy->addError(Game::GAME_STARTED_BY_PLAYER0_ERROR, $playerOProphecy->reveal())->shouldBeCalled();
-        $gameProphecy->addError(Game::GAME_STARTED_BY_PLAYER0_ERROR, $playerXProphecy->reveal());
-        $gameProphecy->errors()->willReturn(Game::GAME_STARTED_BY_PLAYER0_ERROR);
-        $historyProphecy = $this->prophesize(HistoryInterface::class);
-        $historyProphecy->lastItem($gameProphecy->reveal())->willReturn(null);
-        $historyProphecy->saveTurn($playerOProphecy->reveal(), $tileProphecy->reveal(), $gameProphecy->reveal());
-        $historyProphecy->getStartingPlayerSymbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
-        $service = new TakeTileService($gameProphecy->reveal(), $historyProphecy->reveal());
-
-        self::assertSame($boardProphecy->reveal(), $gameProphecy->reveal()->board());
-
-        $service->takeTile($playerOProphecy->reveal(), $tileProphecy->reveal());
-        self::assertTrue($service->hasError(Game::GAME_STARTED_BY_PLAYER0_ERROR));
+        return array($playerXProphecy, $playerOProphecy);
     }
 
     /**
-     * by pozbyć się powtórzeń z hasError mogę użyć Helpera: TurnControl, ale do tego jest mi potrezbna historia
-     * @test
+     * @param $boardProphecy
+     * @param $playerOProphecy
+     * @param $playerXProphecy
+     * @return Game|ObjectProphecy
+     * @throws NotAllowedSymbolValue
      */
-    public function trackHistory()
+    private function prepareGame($boardProphecy, $playerOProphecy, $playerXProphecy)
     {
         $gameProphecy = $this->prophesize(Game::class);
-        $boardProphecy = $this->prophesize(Board::class);
-        $playerXProphecy = $this->prophesize(Player::class);
-        $playerOProphecy = $this->prophesize(Player::class);
-        $tileProphecy = $this->prophesize(Tile::class);
-
-        $playerXProphecy->symbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
-
-        $playerOProphecy->uuid()->willReturn(0);
-        $playerOProphecy->symbolValue()->willReturn(Symbol::PLAYER_0_SYMBOL);
-
         $gameProphecy->board()->willReturn($boardProphecy->reveal());
         $gameProphecy->players()->willReturn([
             $playerOProphecy->reveal(),
             $playerXProphecy->reveal(),
         ]);
-        $gameProphecy->addError(Game::GAME_STARTED_BY_PLAYER0_ERROR, $playerOProphecy->reveal())->shouldBeCalled();
-        $gameProphecy->errors()->willReturn(Game::GAME_STARTED_BY_PLAYER0_ERROR);
+        return $gameProphecy;
+    }
 
-        $historyProphecy = $this->prophesize(HistoryInterface::class);
-        $historyProphecy->lastItem($gameProphecy->reveal())->willReturn(null);
-        $historyProphecy->saveTurn($playerOProphecy->reveal(), $tileProphecy->reveal(), $gameProphecy->reveal())->shouldBeCalled();
-        $historyProphecy->getStartingPlayerSymbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
-        $service = new TakeTileService($gameProphecy->reveal(), $historyProphecy->reveal());
-
-        self::assertSame($boardProphecy->reveal(), $gameProphecy->reveal()->board());
-
-        $service->takeTile($playerOProphecy->reveal(), $tileProphecy->reveal());
+    /**
+     * @param $historyProphecy
+     * @param $playerOProphecy
+     * @param $tileProphecy
+     * @param $gameProphecy
+     */
+    private function prepareSavingTurn($historyProphecy, $playerOProphecy, $tileProphecy, $gameProphecy): void
+    {
+        $historyProphecy->saveTurn($playerOProphecy->reveal(), $tileProphecy->reveal(), $gameProphecy->reveal());
     }
 }
