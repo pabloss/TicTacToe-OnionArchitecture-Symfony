a0ccccd650c4a92b4ef723d152047fedc7800d7b
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2019-06-19 22:40:26 +0200

    player registry

---
+++ src/Core/Application/Service/PlayerRegistry.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Core\Application\Service;
+
+use App\Core\Domain\Model\TicTacToe\Game\Game;
+use App\Core\Domain\Model\TicTacToe\Game\Player;
+
+/**
+ * Class PlayerRegistry
+ * @package App\Core\Application\Service
+ */
+class PlayerRegistry
+{
+    /**
+     * @var array
+     */
+    private $registry = [];
+
+    /**
+     * @param Player $player
+     * @param Game $game
+     */
+    public function registerPlayer(Player $player, Game $game): void
+    {
+        $this->registry[$game->uuid()][$player->symbolValue()] = $player->uuid();
+    }
+
+    /**
+     * @param Game $game
+     * @return array
+     */
+    public function players(Game $game): array
+    {
+        return $this->registry[$game->uuid()];
+    }
+}
---
+++ tests/unit/PlayerRegistryTest.php
@@ -0,0 +1,58 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\unit\History;
+
+use App\Core\Application\Service\PlayerRegistry;
+use App\Core\Domain\Model\TicTacToe\Game\Game;
+use App\Core\Domain\Model\TicTacToe\Game\Player;
+use App\Core\Domain\Model\TicTacToe\ValueObject\Symbol;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class PlayerRegistryTest
+ * @package App\Tests\unit\History
+ */
+class PlayerRegistryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function playersShouldBeRegistered()
+    {
+        // Given
+        // create Player X
+        $playerXProphecy = $this->prophesize(Player::class);
+        $playerXProphecy->uuid()->willReturn(0);
+        $playerXProphecy->symbolValue()->willReturn(Symbol::PLAYER_X_SYMBOL);
+        // create Player O
+        $playerOProphecy = $this->prophesize(Player::class);
+        $playerOProphecy->uuid()->willReturn(1);
+        $playerOProphecy->symbolValue()->willReturn(Symbol::PLAYER_0_SYMBOL);
+        // create game
+        $gameProphecy = $this->prophesize(Game::class);
+        $gameProphecy->uuid()->willReturn(2);
+
+        // When
+        $playerRegistryService = new PlayerRegistry();
+
+        // register both
+        $playerRegistryService->registerPlayer(
+            $playerXProphecy->reveal(),
+            $gameProphecy->reveal()
+        );
+        $playerRegistryService->registerPlayer(
+            $playerOProphecy->reveal(),
+            $gameProphecy->reveal()
+        );
+
+        // Then
+        // take them
+        // they are the same for game
+
+        self::assertSame($playerXProphecy->reveal()->uuid(),
+            $playerRegistryService->players($gameProphecy->reveal())[Symbol::PLAYER_X_SYMBOL]);
+        self::assertSame($playerOProphecy->reveal()->uuid(),
+            $playerRegistryService->players($gameProphecy->reveal())[Symbol::PLAYER_0_SYMBOL]);
+    }
+}
