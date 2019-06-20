26759cafa5a36c7da579012647e476e2e20bd755
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2019-06-20 20:09:52 +0200

    Take Tile Service with Error Log

--- tests/integration/TakeTileServiceTest.php
+++ tests/integration/TakeTileServiceTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Tests\integration;
 
+use App\Core\Application\Errors\ErrorLog;
 use App\Core\Application\Service\PlayerRegistry;
 use App\Core\Application\Service\TakeTileService;
 use App\Core\Application\Validation\TurnControl;
@@ -17,24 +18,26 @@ use PHPUnit\Framework\TestCase;
 class TakeTileServiceTest extends TestCase
 {
 
-    //todo: dorób błędy dla niezarejstrowancyh playerów
     /**
      * @test
      */
     public function takeTile()
     {
+        $errorLog = new ErrorLog();
+
         $playerX = new Player(new Symbol(Symbol::PLAYER_X_SYMBOL), \uniqid());
         $playerO = new Player(new Symbol(Symbol::PLAYER_0_SYMBOL), \uniqid());
         $game = new Game(new Board());
         $playerRegistry = new PlayerRegistry();
         $playerRegistry->registerPlayer($playerX, $game);
         $playerRegistry->registerPlayer($playerO, $game);
-        $turnControl = new TurnControl($playerRegistry);
+        $turnControl = new TurnControl($playerRegistry, $errorLog);
         $service = new TakeTileService($game, new History(), $turnControl);
 
         $tile = new Tile(0, 0);
         $service->takeTile($playerX, $tile);
         self::assertEquals($playerX, $game->board()->getPlayer($tile));
+        self::assertTrue($errorLog->noErrors($game));
     }
 
     /**
@@ -42,18 +45,20 @@ class TakeTileServiceTest extends TestCase
      */
     public function takeTileByWrongPlayer()
     {
+        $errorLog = new ErrorLog();
         $playerO = new Player(new Symbol(Symbol::PLAYER_0_SYMBOL), \uniqid());
         $playerX = new Player(new Symbol(Symbol::PLAYER_X_SYMBOL), \uniqid());
         $game = new Game(new Board());
         $playerRegistry = new PlayerRegistry();
         $playerRegistry->registerPlayer($playerX, $game);
         $playerRegistry->registerPlayer($playerO, $game);
-        $turnControl = new TurnControl($playerRegistry);
+        $turnControl = new TurnControl($playerRegistry, $errorLog);
         $service = new TakeTileService($game, new History(), $turnControl);
 
         $tile = new Tile(0, 0);
         $service->takeTile($playerO, $tile);
-        self::assertTrue($service->hasError(Game::GAME_STARTED_BY_PLAYER0_ERROR));
+        self::assertFalse($errorLog->noErrors($game));
+        self::assertTrue($errorLog->hasError(Game::GAME_STARTED_BY_PLAYER0_ERROR, $game));
     }
 
     /**
@@ -61,16 +66,18 @@ class TakeTileServiceTest extends TestCase
      */
     public function takeTileByNonRegisteredPlayer()
     {
+        $errorLog = new ErrorLog();
         $playerO = new Player(new Symbol(Symbol::PLAYER_0_SYMBOL), \uniqid());
         $game = new Game(new Board());
         $playerRegistry = new PlayerRegistry();
-        $turnControl = new TurnControl($playerRegistry);
+        $turnControl = new TurnControl($playerRegistry, $errorLog);
         $service = new TakeTileService($game, new History(), $turnControl);
 
         $tile = new Tile(0, 0);
         $service->takeTile($playerO, $tile);
-        self::assertTrue($service->hasError(Game::PLAYER_IS_NOT_ALLOWED));
-        self::assertTrue($service->hasError(Game::GAME_STARTED_BY_PLAYER0_ERROR));
+        self::assertFalse($errorLog->noErrors($game));
+        self::assertTrue($errorLog->hasError(Game::PLAYER_IS_NOT_ALLOWED, $game));
+        self::assertTrue($errorLog->hasError(Game::GAME_STARTED_BY_PLAYER0_ERROR, $game));
     }
 
     /**
@@ -78,16 +85,18 @@ class TakeTileServiceTest extends TestCase
      */
     public function takeTileByDuplacatedMove()
     {
+        $errorLog = new ErrorLog();
         $playerO = new Player(new Symbol(Symbol::PLAYER_0_SYMBOL), \uniqid());
         $game = new Game(new Board());
         $playerRegistry = new PlayerRegistry();
-        $turnControl = new TurnControl($playerRegistry);
+        $turnControl = new TurnControl($playerRegistry, $errorLog);
         $playerRegistry->registerPlayer($playerO, $game);
         $service = new TakeTileService($game, new History(), $turnControl);
 
         $tile = new Tile(0, 0);
         $service->takeTile($playerO, $tile);
         $service->takeTile($playerO, $tile);
-        self::assertTrue($service->hasError(Game::DUPLICATED_TURNS_ERROR));
+        self::assertFalse($errorLog->noErrors($game));
+        self::assertTrue($errorLog->hasError(Game::DUPLICATED_TURNS_ERROR, $game));
     }
 }
