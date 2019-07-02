e997cc908a8063ac822df8ce9e0159737813470f
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2019-07-02 23:52:47 +0200

    Player Test adapted to refactor

--- tests/integration/PlayerTest.php
+++ tests/integration/PlayerTest.php
@@ -3,14 +3,19 @@ declare(strict_types=1);
 
 namespace App\Tests\integration;
 
+use App\Core\Application\Errors\ErrorLog;
 use App\Core\Application\Event\EventManager;
 use App\Core\Application\Event\EventSubscriber\TakeTileEventSubscriber;
+use App\Core\Application\Service\PlayerRegistry;
+use App\Core\Application\Service\TakeTileService;
+use App\Core\Application\Validation\TurnControl;
 use App\Core\Domain\Event\EventInterface;
 use App\Core\Domain\Model\TicTacToe\AI\AI;
 use App\Core\Domain\Model\TicTacToe\Game\Board;
 use App\Core\Domain\Model\TicTacToe\Game\Game as TicTacToe;
 use App\Core\Domain\Model\TicTacToe\Game\History;
 use App\Core\Domain\Model\TicTacToe\ValueObject\Symbol;
+use App\Core\Domain\Model\TicTacToe\ValueObject\Tile;
 use App\Core\Domain\Service\FindWinner;
 use App\Core\Domain\Service\PlayersFactory;
 use App\Presentation\Web\Pub\Event\Event;
@@ -18,38 +23,56 @@ use PHPUnit\Framework\TestCase;
 
 class PlayerTest extends TestCase
 {
-    /** @var  TicTacToe $game */
+    /** @var \App\Entity\Player[] */
+    private $players;
+    /** @var TicTacToe */
     private $game;
+    /** @var TurnControl */
+    private $turnControl;
+
+    /** @var ErrorLog */
+    private $errorLog;
+
+    protected function setUp()
+    {
+        $playerRegistry = new PlayerRegistry();
+        $playersFactory = new PlayersFactory();
+        $this->players = $playersFactory->create();
+        $this->game = new TicTacToe(new Board());
+        $playerRegistry->registerPlayer(
+            $this->players[Symbol::PLAYER_X_SYMBOL],
+            $this->game
+        );
+        $playerRegistry->registerPlayer(
+            $this->players[Symbol::PLAYER_0_SYMBOL],
+            $this->game
+        );
+        $errorLog = new ErrorLog();
+        $this->errorLog = $errorLog;
+
+        $this->turnControl = new TurnControl($playerRegistry, $this->errorLog);
+    }
 
     /**
      * @test
      */
     public function looping_AI_player_fills_whole_board_in_9_turns()
     {
-        $history = new History();
-        TakeTileEventSubscriber::$counter = 0;
-        $subscriber = new TakeTileEventSubscriber($history);
-        $eventManager = EventManager::getInstance();
-        $eventManager->detach(Event::NAME);
-        $eventManager->attach(Event::NAME, function (EventInterface $event) use ($subscriber) {
-            $subscriber->onTakenTile($event);
-        });
-        $game = new TicTacToe(new Board(), new PlayersFactory(), new FindWinner(),
-            $eventManager,
-            \uniqid()
-            );
+        $game  = $this->game;
         $ai = new AI($game);
-        $this->game = $game;
-        list(Symbol::PLAYER_X_SYMBOL => $player, Symbol::PLAYER_0_SYMBOL => $notUsedPlayer) = $game->players();
+        $history = new History();
+        $takeTileService = new TakeTileService($this->game, $history, $this->turnControl);
+
+        list(Symbol::PLAYER_X_SYMBOL => $player, Symbol::PLAYER_0_SYMBOL => $notUsedPlayer) = $this->players;
         for (
             $expectedFilledCount = 2;
             $expectedFilledCount <= 9;
             $expectedFilledCount += 2
         ) {
-            $player->takeTile($ai->takeRandomFreeTile(), $game, $history);
-            $notUsedPlayer->takeTile($this->simulate_choosing_tiles_of_real_player(), $game, $history);
+            $takeTileService->takeTile($player, $ai->takeRandomFreeTile());
+            $takeTileService->takeTile($notUsedPlayer, $this->simulate_choosing_tiles_of_real_player());
             $actualFilledCount = \array_reduce(
-                $game->board()->contents(),
+                $this->game->board()->contents(),
                 function ($carry, $item) {
                     if (null !== $item) {
                         $carry++;
@@ -63,7 +86,7 @@ class PlayerTest extends TestCase
             $allFreeTileIndexes = [];
             $allFilledTileIndexes = [];
             \array_walk(
-                $game->board()->contents(),
+                $this->game->board()->contents(),
                 function ($value, $key) use (&$allFreeTileIndexes, &$allFilledTileIndexes) {
                     if (null === $value) {
                         $allFreeTileIndexes[] = $key;
