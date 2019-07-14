3e28e0b8b20fc040f6e399f70497d81047d1e6b6
Paweł Zimnowodzki <p.zimnowodzki@moonfox.pl>
2019-07-14 18:09:51 +0200

    Plaing against AI applied to refactor

--- tests/integration/PlayingAgainstAISimulationTest.php
+++ tests/integration/PlayingAgainstAISimulationTest.php
@@ -3,7 +3,10 @@ declare(strict_types=1);
 
 namespace App\Tests\integration\business;
 
-use App\Core\Application\Event\EventManager;
+use App\Core\Application\Errors\ErrorLog;
+use App\Core\Application\Service\PlayerRegistry;
+use App\Core\Application\Service\TakeTileService;
+use App\Core\Application\Validation\TurnControl;
 use App\Core\Domain\Model\TicTacToe\AI\AI;
 use App\Core\Domain\Model\TicTacToe\Game\Board;
 use App\Core\Domain\Model\TicTacToe\Game\Game as TicTacToe;
@@ -16,37 +19,56 @@ use PHPUnit\Framework\TestCase;
 
 class PlayingAgainstAISimulationTest extends TestCase
 {
-    /** @var  TicTacToe $game */
+    /** @var \App\Entity\Player[] */
+    private $players;
+    /** @var TicTacToe */
     private $game;
+    /** @var TurnControl */
+    private $turnControl;
 
+    protected function setUp()
+    {
+        $playerRegistry = new PlayerRegistry();
+        $playersFactory = new PlayersFactory();
+        $this->players = $playersFactory->create();
+        $this->game = new TicTacToe(new Board());
+        $playerRegistry->registerPlayer(
+            $this->players[Symbol::PLAYER_X_SYMBOL],
+            $this->game
+        );
+        $playerRegistry->registerPlayer(
+            $this->players[Symbol::PLAYER_0_SYMBOL],
+            $this->game
+        );
+        $errorLog = new ErrorLog();
+
+        $this->turnControl = new TurnControl($playerRegistry, $errorLog);
+    }
     /**
      * @test
      */
     public function random_looped_taken_tiles_should_fill_whole_board()
     {
-        $eventManager = EventManager::getInstance();
         $history = new History();
-        $game = new TicTacToe(new Board(), new PlayersFactory(), new FindWinner(),
-            $eventManager,
-            \uniqid()
-            );
-        $this->game = $game;
-        list(Symbol::PLAYER_X_SYMBOL => $playerX, Symbol::PLAYER_0_SYMBOL => $player0) = $game->players();
-        $ai = new AI($game);
+        $playersFactory = new PlayersFactory();
+        $findWinnerService = new FindWinner();
+        $takeTileService = new TakeTileService($this->game, $history, $this->turnControl);
+        list(Symbol::PLAYER_X_SYMBOL => $playerX, Symbol::PLAYER_0_SYMBOL => $player0) = $playersFactory->create();;
+        $ai = new AI($this->game);
         for ($i = 2; $i <= 9; $i += 2) {
-            $playerX->takeTile($ai->takeRandomFreeTile(), $game, $history);
+            $takeTileService->takeTile($playerX, $ai->takeRandomFreeTile());
             /** @var Player $player0 */
-            $player0->takeTile($this->simulate_choosing_tiles_of_real_player(), $game, $history);
+            $takeTileService->takeTile($player0, $this->simulate_choosing_tiles_of_real_player());
         }
 
         self::assertTrue(
-            \is_null($game->winner()) ||
-            $game->winner()->symbol()->value() === 'X' ||
-            $game->winner()->symbol()->value() === '0'
+            \is_null($findWinnerService->winner($this->game)) ||
+            $findWinnerService->winner($this->game)->symbol()->value() === 'X' ||
+            $findWinnerService->winner($this->game)->symbol()->value() === '0'
         );
         self::assertTrue(
             \array_reduce(
-                $game->board()->contents(),
+                $this->game->board()->contents(),
                 function ($carry, $value) {
                     $carry = $carry || (\is_null($value) === false);
 
